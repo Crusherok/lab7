f8b8444f55718c1c98350324200fe4676fd719be
Crusherok <158302408+Crusherok@users.noreply.github.com>
2025-04-21 15:28:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./index.css";
 import pic from "./assets/y.jpg"; // Import the image
 
 const ProfileCard = ({ name, bio, initialBgColor }) => {
